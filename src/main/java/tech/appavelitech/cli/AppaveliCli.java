d0a8fc2272b168311dd24716437009e774cf3f75
Dominque Terry <terry_dominque@yahoo.com>
2025-04-18 16:31:49 -0400

    feat: generate DBConnection using jdbc (#2)

--- src/main/java/tech/appavelitech/cli/AppaveliCli.java
+++ src/main/java/tech/appavelitech/cli/AppaveliCli.java
@@ -15,53 +15,31 @@
  */
 package tech.appavelitech.cli;
 
+import java.util.Properties;
+
 public class AppaveliCli {
     public static void main(String[] args) {
-        if (args.length < 3) {
+        if (args.length < 1) {
             printUsage();
             return;
         }
 
         String command = args[0];
-        String entity = null;
-        String basePackage = null;
-        String fields = null;
 
-        for (int i = 1; i < args.length; i++) {
-            switch (args[i]) {
-                case "--entity":
-                    entity = args[i + 1];
-                    i++;
-                    break;
-                case "--package":
-                    basePackage = args[i + 1];
-                    i++;
-                    break;
-                case "--fields":
-                    fields = args[i + 1];
-                    i++;
-                    break;
-            }
-        }
-
-        if (entity == null || basePackage == null) {
-            System.out.println("Missing required parameters.");
-            printUsage();
-            return;
-        }
+        // Load defaults from config file
+        Properties config = ConfigLoader.load(".appaveli-config.properties");
 
         switch (command) {
             case "generate-dao":
-                DaoGenerator.generate(entity, basePackage);
+                handleGenerateDao(args);
                 break;
 
             case "generate-domain":
-                if (fields == null) {
-                    System.out.println("--fields is required for generate-domain");
-                    printUsage();
-                    return;
-                }
-                DomainGenerator.generate(entity, basePackage, fields);
+                handleGenerateDomain(args);
+                break;
+
+            case "generate-jdbc":
+                handleGenerateJdbc(args, config);
                 break;
 
             default:
@@ -70,9 +48,100 @@ public class AppaveliCli {
         }
     }
 
+    private static void handleGenerateDao(String[] args) {
+        String entity = null;
+        String basePackage = null;
+
+        for (int i = 1; i < args.length; i++) {
+            switch (args[i]) {
+                case "--entity":
+                    if (i + 1 < args.length) entity = args[++i];
+                    break;
+                case "--package":
+                    if (i + 1 < args.length) basePackage = args[++i];
+                    break;
+            }
+        }
+
+        if (entity == null || basePackage == null) {
+            System.out.println("Missing required parameters for generate-dao.");
+            printUsage();
+            return;
+        }
+
+        DaoGenerator.generate(entity, basePackage);
+    }
+
+    private static void handleGenerateDomain(String[] args) {
+        String entity = null;
+        String basePackage = null;
+        String fields = null;
+
+        for (int i = 1; i < args.length; i++) {
+            switch (args[i]) {
+                case "--entity":
+                    if (i + 1 < args.length) entity = args[++i];
+                    break;
+                case "--package":
+                    if (i + 1 < args.length) basePackage = args[++i];
+                    break;
+                case "--fields":
+                    if (i + 1 < args.length) fields = args[++i];
+                    break;
+            }
+        }
+
+        if (entity == null || basePackage == null || fields == null) {
+            System.out.println("Missing required parameters for generate-domain.");
+            printUsage();
+            return;
+        }
+
+        DomainGenerator.generate(entity, basePackage, fields);
+    }
+
+    private static void handleGenerateJdbc(String[] args, Properties config) {
+        String basePackage = null;
+        String dbName = config.getProperty("db.name", "your_database");
+        String dbUser = config.getProperty("db.user", "your_user");
+        String dbPassword = config.getProperty("db.password", "your_password");
+
+        for (int i = 1; i < args.length; i++) {
+            switch (args[i]) {
+                case "--package":
+                    if (i + 1 < args.length) basePackage = args[++i];
+                    break;
+                case "--db-name":
+                    if (i + 1 < args.length) dbName = args[++i];
+                    break;
+                case "--db-user":
+                    if (i + 1 < args.length) dbUser = args[++i];
+                    break;
+                case "--db-password":
+                    if (i + 1 < args.length) dbPassword = args[++i];
+                    break;
+                default:
+                    System.out.println("Unknown argument for generate-jdbc: " + args[i]);
+                    printUsage();
+                    return;
+            }
+        }
+
+        if (basePackage == null) {
+            System.out.println("Missing required --package for generate-jdbc.");
+            printUsage();
+            return;
+        }
+
+        System.out.println("[DEBUG] Calling JdbcUtilGenerator...");
+        JdbcUtilGenerator.generate(basePackage, dbName, dbUser, dbPassword);
+    }
+
     private static void printUsage() {
         System.out.println("\nUsage:");
         System.out.println("  generate-dao --entity EntityName --package your.package.name");
-        System.out.println("  generate-domain --entity EntityName --package your.package.name --fields \"name:String,email:String\"\n");
+        System.out.println("  generate-domain --entity EntityName --package your.package.name --fields \"name:String,email:String\"");
+        System.out.println("  generate-jdbc --package your.package.name [--db-name db] [--db-user user] [--db-password pass]");
+        System.out.println("\nOptional: define defaults in .appaveli-config.properties\n");
     }
 }
---
+++ src/main/java/tech/appavelitech/cli/JdbcUtilGenerator.java
@@ -0,0 +1,48 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2024 AppaveliTech Solutions, LLC
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ */
+package tech.appavelitech.cli;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Map;
+
+public class JdbcUtilGenerator {
+    public static void generate(String basePackage, String dbName, String dbUser, String dbPassword) {
+        String content = TemplateEngine.render(
+                "templates/jdbcutils.template",
+                Map.of(
+                        "PACKAGE", basePackage,
+                        "DB_NAME", dbName,
+                        "DB_USER", dbUser,
+                        "DB_PASSWORD", dbPassword
+                )
+        );
+
+        String outputDir = "generated/" + basePackage.replace('.', '/');
+        new File(outputDir).mkdirs();
+
+        writeToFile(outputDir + "/DatabaseConnection.java", content);
+    }
+
+    private static void writeToFile(String path, String content) {
+        try (FileWriter writer = new FileWriter(path)) {
+            writer.write(content);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
