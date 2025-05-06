5def9097aa7c9ce7dc7ab91658d13fe1dee0652d
Dominque Terry <terry_dominque@yahoo.com>
2025-05-06 17:21:16 -0400

    chore: modify pom and package name

--- src/main/java/io/github/appaveli/cli/ServiceGenerator.java
+++ src/main/java/io/github/appaveli/cli/ServiceGenerator.java
@@ -1,22 +1,19 @@
-package tech.appavelitech.cli;
+package io.github.appaveli.cli;
 
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
-public class ServletGenerator {
-
+public class ServiceGenerator {
     public static void generate(String entity, String basePackage) {
-        String templatePath = "templates/Servlet.template";
-        String content = TemplateEngine.render(templatePath, entity, basePackage);
+        String interfaceContent = TemplateEngine.render("templates/ServiceInterface.template", entity, basePackage);
+        String implContent = TemplateEngine.render("templates/ServiceImpl.template", entity, basePackage);
 
         String outputDir = "generated/" + basePackage.replace('.', '/');
         new File(outputDir).mkdirs();
 
-        String outputPath = outputDir + "/" + entity + "Servlet.java";
-        writeToFile(outputPath, content);
-
-        System.out.println("Servlet generated at: " + outputPath);
+        writeToFile(outputDir + "/" + entity + "Service.java", interfaceContent);
+        writeToFile(outputDir + "/" + entity + "ServiceImpl.java", implContent);
     }
 
     private static void writeToFile(String path, String content) {
